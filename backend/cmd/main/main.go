68c49f68e9863d42c50b64a52e6c34db2f3ff736
Prathamesh Theurkar <prathameshtheurkar037@gmail.com>
2024-05-20 13:34:00 +0530

    Completed backend

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -4,19 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PrathameshTheurkar/go-todoapp/backend/database"
 	"github.com/PrathameshTheurkar/go-todoapp/backend/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error while loading env")
+		panic(err)
+	}
 	database.Connect()
 	router := mux.NewRouter()
 
 	routes.AllRoutes(router)
 
 	fmt.Println("Server is running on port: 4000")
-	log.Fatal(http.ListenAndServe(":4000", router))
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), router))
 }
